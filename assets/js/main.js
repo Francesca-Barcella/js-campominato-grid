d0953f92bb398132ce919e5de3371fff98fa2550
Francesca-Barcella <pollon.fra@gmail>
2022-10-17 15:39:20 +0200

    JS - push per ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-//alert('ciao, funziono!')
+
+alert('ciao, funziono!')
 
 /******************************************************************* 
 TRACCIA
@@ -7,9 +8,37 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 *******************************************************************/
 
-// creo il contenitore in html in cui verranno inserite le celle
-// creo il pulsante che l'utente dovrà cliccare per generare la griglia di gioco
-
 /* 
  <div class="cell"></div>
 */
+
+// collego il container del dom a js
+// creo le variabili per aggiungere 100 celle con let/const e getElementsByClassName
+let containerGrid = document.querySelector('.container');
+console.log(containerGrid);
+
+let cell = '<div class="cell"></div>';
+console.log(cell);
+
+let totalCells = 100;
+console.log(totalCells);
+
+let listCells = document.getElementsByClassName('cell');
+console.log(listCells);
+
+
+//aggiungo le 100 in console
+for (let i = 0; i < totalCells; i++) {
+    containerGrid += cell;
+}
+console.log(containerGrid);
+
+for (let i = 0; i < listCells.length; i++) {
+
+    let choiceCell = listCells[i];
+
+    choiceCell.addEventListener('click', function () {
+        choiceCell.classList.add('blue');
+    });
+
+}
