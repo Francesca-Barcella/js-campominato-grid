8eb08a827afd1e8abcd3ecc12282f63f35f26e3d
Francesca-Barcella <pollon.fra@gmail>
2022-10-17 14:14:32 +0200

    JS-CG container e button html+css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-alert('ciao, funziono!')
+//alert('ciao, funziono!')
+
+/******************************************************************* 
+TRACCIA
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*******************************************************************/
+
+// creo il contenitore in html in cui verranno inserite le celle
+// creo il pulsante che l'utente dovrà cliccare per generare la griglia di gioco
+
+/* 
+ <div class="cell"></div>
+*/
